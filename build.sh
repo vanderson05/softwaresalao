ec1bc91e20e50f2da81db0d52d212a3da6755978
Vanderson <vanderson0511@gmail.com>
2026-05-21 19:05:34 -0300

    Cria admin inicial

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='vanderson').exists() or User.objects.create_superuser('vanderson','vanderson0511@gmail.com','vanderson')
+"
